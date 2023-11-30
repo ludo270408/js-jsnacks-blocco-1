4265f6a5803976f86d43baf2c71b4c6ef5b2c717
ludo270408 <ludovica.sette7@gmaio.com>
2023-12-01 00:04:58 +0100

    [UPTADE] Peach putted in the "fridge"

--- script.js
+++ script.js
@@ -1,2 +1,6 @@
 //Chiamiamo l'array frigorifero(contiene la frutta)
-let frigoriferto =['banana', 'mela', 'pera', 'ciliegia', 'arancia', 'mandarino', 'cocomero', 'limone', 'fragola' ]
+let frigoriferto =['banana', 'mela', 'pera', 'ciliegia', 'arancia', 'mandarino', 'cocomero', 'limone', 'fragola' ];
+//Notiamo la pesca sul tavolo
+let peach = 'pesca';
+//Mettiamo la pesca nel frigorifero
+frigoriferto.push(peach);
