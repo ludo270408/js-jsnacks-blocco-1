9abc210bda830866b6e86b8516cd6dbdd374b888
ludo270408 <ludovica.sette7@gmaio.com>
2023-12-01 00:10:38 +0100

    [UPTADE] First part done

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 //Chiamiamo l'array frigorifero(contiene la frutta)
-let frigoriferto =['banana', 'mela', 'pera', 'ciliegia', 'arancia', 'mandarino', 'cocomero', 'limone', 'fragola' ];
+let fridge =['banana', 'mela', 'pera', 'ciliegia', 'arancia', 'mandarino', 'cocomero', 'limone', 'fragola' ];
 //Notiamo la pesca sul tavolo
 let peach = 'pesca';
 //Mettiamo la pesca nel frigorifero
-frigoriferto.push(peach);
+fridge.push(peach);
+
+//Controllo se ho il cocomero in frigo per fare i cocktail
+for(let i=0; i<fridge.length; i++){
+    //é il cocomero? si/no
+    if(fridge[i]=='cocomero'){
+        console.log('Trovato! Devo solo preparare il cocktail.');
+    }
+    else{
+        console.log('Oh no, devo uscire a comprare il cocomero!');
+    }
+}
